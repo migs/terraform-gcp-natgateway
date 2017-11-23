4a00c84fc8ddab95140ebcefb0adf2b32a28563e
Stuart Moore <stuart.moore@gmail.com>
2017-11-23 16:14:15 +0000

    Removing managed instance group, reverting to normal instances

--- variables.tf
+++ variables.tf
@@ -12,16 +12,12 @@ variable "ha" { default = false }
 variable "region_params" {
   type = "map"
   default = {
-    europe-west1 {
-      zone1 = "europe-west1-b"
-      zone2 = "europe-west1-c"
-      zone3 = "europe-west1-d"
-    }
-    europe-west2 {
-      zone1 = "europe-west2-a"
-      zone2 = "europe-west2-b"
-      zone3 = "europe-west2-c"
-    }
+    europe-west1.zone1  = "europe-west1-b"
+    europe-west1.zone2  = "europe-west1-c"
+    europe-west1.zone3  = "europe-west1-d"
+    europe-west2.zone1  = "europe-west2-a"
+    europe-west2.zone2  = "europe-west2-b"
+    europe-west2.zone3  = "europe-west2-c"
   }
 }
 variable "priority" { default = "800" }
