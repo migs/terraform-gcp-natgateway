c540806a70fae9dbcfef266950de2dc4db58c4ea
Stuart Moore <stuart.moore@gmail.com>
2017-11-23 10:34:47 +0000

    First draft

--- managed-instance-group.tf
+++ managed-instance-group.tf
@@ -123,7 +123,7 @@ resource "google_compute_route" "nat-gateway-zone1" {
   dest_range = "0.0.0.0/0"
   network = "${var.network}"
   next_hop_instance = "${element(split("/", google_compute_instance_group.nat-gateway-zone1.instances[0]), 10)}"
-  next_hop_instance_zone = "${lookup(var.region_params["${var.region}",zone1)}"
+  next_hop_instance_zone = "${lookup(var.region_params["${var.region}"],zone1)}"
   tags = ["${var.tags}"]
   priority = "${var.priority}"  
 }
@@ -134,7 +134,7 @@ resource "google_compute_route" "nat-gateway-zone2" {
   dest_range = "0.0.0.0/0"
   network = "${var.network}"
   next_hop_instance = "${element(split("/", google_compute_instance_group.nat-gateway-zone2.instances[0]), 10)}"
-  next_hop_instance_zone = "${lookup(var.region_params["${var.region}",zone2)}"
+  next_hop_instance_zone = "${lookup(var.region_params["${var.region}"],zone2)}"
   tags = ["${var.tags}"]
   priority = "${var.priority}"  
 }
@@ -145,7 +145,7 @@ resource "google_compute_route" "nat-gateway-zone3" {
   dest_range = "0.0.0.0/0"
   network = "${var.network}"
   next_hop_instance = "${element(split("/", google_compute_instance_group.nat-gateway-zone3.instances[0]), 10)}"
-  next_hop_instance_zone = "${lookup(var.region_params["${var.region}",zone3)}"
+  next_hop_instance_zone = "${lookup(var.region_params["${var.region}"],zone3)}"
   tags = ["${var.tags}"]
   priority = "${var.priority}"  
 }
