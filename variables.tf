e3eb62f042a4a05f4b515d61111875bb86f3e339
Stuart Moore <stuart.moore@gmail.com>
2017-11-28 10:35:41 +0000

    I will be amazed if this actually works ...

--- variables.tf
+++ variables.tf
@@ -43,10 +43,10 @@ variable "region_params" {
       zone2 = "europe-west1-c"
       zone3 = "europe-west1-d"
     }
-    europe-west2 = {
-      ["europe-west2-a"],
-      ["europe-west2-b"],
-      ["europe-west2-c"],
+    europe-west2 {
+      zone1 = "europe-west2-a"
+      zone2 = "europe-west2-b"
+      zone3 = "europe-west2-c"
     }
     europe-west3 {
       zone1 = "europe-west3-a"
